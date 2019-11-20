9ef2efeafd6ba818639c1ec74a1525f18143dfa7
Alexandru Antone <Alexandru.Antone@enea.com>
2019-11-20 18:08:18 +0200

    WIP: evaluateuate ceph keys.
    Signed-off-by: Alexandru Antone <Alexandru.Antone@enea.com>
    Change-Id: I1f0d73399429ef40f414efeb6931db111eba0faf

--- inventoryhandlers/storageinventory/storageinventory.py
+++ inventoryhandlers/storageinventory/storageinventory.py
@@ -570,7 +570,7 @@ JSON_CEPH_ANSIBLE_MONS_HOST_VARS = """
          "openstack_keys": [
              {
                  "acls": [],
-                 "key": "$(ceph-authtool --gen-print-key)",
+                 "key": "{{ ceph_keys['client.shared'] }}",
                  "mode": "0600",
                  "mon_cap": "allow r",
                  "name": "client.shared",
@@ -578,7 +578,7 @@ JSON_CEPH_ANSIBLE_MONS_HOST_VARS = """
              }{% if is_openstack_deployment %},
              {
                  "acls": [],
-                 "key": "$(ceph-authtool --gen-print-key)",
+                 "key": "{{ ceph_keys['client.glance'] }}",
                  "mode": "0640",
                  "mon_cap": "allow r",
                  "name": "client.glance",
@@ -586,7 +586,7 @@ JSON_CEPH_ANSIBLE_MONS_HOST_VARS = """
              },
              {
                  "acls": [],
-                 "key": "$(ceph-authtool --gen-print-key)",
+                 "key": "{{ ceph_keys['client.cinder'] }}",
                  "mode": "0640",
                  "mon_cap": "allow r, allow command \\\\\\\\\\\\\\"osd blacklist\\\\\\\\\\\\\\"",
                  "name": "client.cinder",
@@ -596,7 +596,7 @@ JSON_CEPH_ANSIBLE_MONS_HOST_VARS = """
         {%- if is_caas_deployment and 0 < osd_pool_caas_pg_num %},
              {
                  "acls": [],
-                 "key": "$(ceph-authtool --gen-print-key)",
+                 "key": "{{ ceph_keys['client.caas'] }}",
                  "mode": "0600",
                  "mon_cap": "allow r",
                  "name": "client.caas",
@@ -888,6 +888,14 @@ class storageinventory(cmansibleinventoryconfig.CMAnsibleInventoryConfigPlugin):
             hosts=self.hosts,
             **self._get_ceph_vars())
 
+    @property
+    def _ceph_keys(self):
+        return {
+            'client.shared': os.system("ceph-authtool --gen-print-key"),
+            'client.glance': os.system("ceph-authtool --gen-print-key"),
+            'client.cinder': os.system("ceph-authtool --gen-print-key"),
+            'client.caas':   os.system("ceph-authtool --gen-print-key")
+        }
     def _get_ceph_vars(self):
         return {
             'osd_pool_images_pg_num':  self._calculated_images_pg_num,
@@ -901,7 +909,8 @@ class storageinventory(cmansibleinventoryconfig.CMAnsibleInventoryConfigPlugin):
             'nova_pool':               self._nova_pool_name,
             'glance_pool':             self._glance_pool_name,
             'cinder_pool':             self._cinder_pool_name,
-            'platform_pool':           self._platform_pool_name
+            'platform_pool':           self._platform_pool_name,
+            'ceph_keys'    :           self._ceph_keys
         }
 
     def _add_ceph_ansible_osds_sample_host_vars(self):
